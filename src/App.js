0e8793cf927a691ca5ed57fb0ce9883d987a0ac7
Wiktor Tuminski <wiktor.tuminski@trip4share.com>
2020-12-11 14:53:31 +0100

    change react-helmet into react-helmet-async

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+// I have changed react-helmet into react-helmet-async, because the first one relies on react-side-effect which uses "UNSAFE_componentWillMount in strict mode". 
 
 import { BrowserRouter as Router } from "react-router-dom";
 import ApplicationRouter from './containers/routes/ApplicationRouter';
@@ -20,17 +21,24 @@ class App extends Component {
         return (
           <ThemeProvider theme={{colors: COLORS}}>
             <Router>
-              <div className="App">
-              <Helmet>
-                  <link rel="preconnect" href="https://fonts.gstatic.com" />
-                  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300&display=swap" rel="stylesheet" />
-                </Helmet>
-                <GlobalStyles />
-                <Header />
-                <main>
-                  <ApplicationRouter />
-                </main>
-              </div>
+              <HelmetProvider >
+                <div className="App">
+                  <Helmet>
+                    <meta
+                          name="description"
+                          content="Photo Gallery created with React"
+                    />
+                    <title>React based Photo Gallery</title>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,300&display=swap" rel="stylesheet" />
+                  </Helmet>
+                  <GlobalStyles />
+                  <Header />
+                  <main>
+                    <ApplicationRouter />
+                  </main>
+                </div>
+              </HelmetProvider>
             </Router>
           </ThemeProvider>
         );
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,5 @@
 import React, { Component }from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { withRouter } from 'react-router-dom';
 import withFetch from '../HOC/withFetch';
 import Card from '../Card';
